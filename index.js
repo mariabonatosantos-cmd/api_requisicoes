b07e3deae9a54096a81c6b78e6912cfb28257362
JuliaSkraba <juliaskraba@gmail.com>
2026-06-17 12:08:52 -0300

    edit

--- index.js
+++ index.js
@@ -10,14 +10,22 @@ app.use(express.json());
 app.use(cors());
 
 const clientesfile = path.join(__dirname, "clientes.json")
-function LerClientes() {
-    if(!)
+function SalvarCliente(clientes) {
+    fs.writeFileSync(clientesfile, JSON.stringify(clientes, null, 2), "utf8")
 }
 
+app.post("/clientes", (req, res) =>{
+    const {nome, cpf, cep, rua, cidade, estado, numero}= req
+})
+
+function LerClientes() {
+    if (!fs.existsSync(clientesfile)) {
+    }}
+
 //http://localhost:3000/saudacao?nome=maria
-app.get("/saudacao", (req, res)=>{
+app.get("/saudacao", (req, res) => {
     const nome = req.query.nome;
-    if(!nome) {
+    if (!nome) {
         return res.status(404).json(
             {
                 erro: "Nome não foi informado"
@@ -33,17 +41,17 @@ app.get("/saudacao", (req, res)=>{
 })
 
 app.post("/cadastrarCliente", (req, res) => {
-    const {nome, cpf, cep} = req.body;
+    const { nome, cpf, cep } = req.body;
 
-    if(!nome || !cpf || !cep){
-       return res.status(404).json({erro: "Dados incompletos"}) 
+    if (!nome || !cpf || !cep) {
+        return res.status(404).json({ erro: "Dados incompletos" })
     }
     res.json({
-        nome, 
+        nome,
         cpf,
         cep
     })
-    
+
 })
 //final
 app.listen(port, () => {
@@ -51,44 +59,43 @@ app.listen(port, () => {
 })
 
 app.post("/media", (req, res) => {
-    const {nota1, nota2} = req.body;
+    const { nota1, nota2 } = req.body;
 
-    if(!nota1 || !nota2){
-       return res.status(404).json({erro: "Dados incompletos"}) 
+    if (!nota1 || !nota2) {
+        return res.status(404).json({ erro: "Dados incompletos" })
     }
 
-    const media = (parseFloat(nota1) + parseFloat(nota2))/ 2;
+    const media = (parseFloat(nota1) + parseFloat(nota2)) / 2;
     res.json({
         nota1,
-        nota2, 
+        nota2,
         mensagem: media >= 7 ? "Aprovado" : "Reprovado",
         media: parseFloat(media)
     })
-    
+
 })
 //final
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`)    
+    console.log(`Servidor rodando em http://localhost:${port}`)
 })
 
 app.post("/login", (req, res) => {
-    const {email, senha} = req.body;
+    const { email, senha } = req.body;
 
-    if(!email || !senha){
-        return res.status(404).json({erro: "Dados incompletos!"})
+    if (!email || !senha) {
+        return res.status(404).json({ erro: "Dados incompletos!" })
     }
 
 
 
-       if (email === "admin@admin.com" && senha === "123456")
-       {
+    if (email === "admin@admin.com" && senha === "123456") {
         return res.json({
             email,
             senha,
             token: "123456"
         })
-       }
-         else {
-            return res.status(404).json({erro: "Credenciais inválidas!"})
-         }
+    }
+    else {
+        return res.status(404).json({ erro: "Credenciais inválidas!" })
+    }
 })
